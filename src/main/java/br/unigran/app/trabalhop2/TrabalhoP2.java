768b58f3243d900f795495714594efc0ee2f0315
Pedro <pedrophnc@gmail.com>
2024-05-02 15:40:37 -0400

    third

--- src/main/java/br/unigran/app/trabalhop2/TrabalhoP2.java
+++ src/main/java/br/unigran/app/trabalhop2/TrabalhoP2.java
@@ -11,6 +11,6 @@ package br.unigran.app.trabalhop2;
 public class TrabalhoP2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Trabalho");
     }
 }
